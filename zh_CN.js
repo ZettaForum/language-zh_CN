4c6ea55f9898e0b1da799542dda4f613aa82e07d
Xmader <xmader@outlook.com>
2018-12-12 21:31:26 +0800

    更新简体中文语言包

--- zh_CN.js
+++ zh_CN.js
@@ -1,7 +1,7 @@
 
 export const translations = {
     "404page": {
-        "page_not_found": "你访问的页面不存在",
+        "page_not_found": "你访问的页面不存在，",
         "please": "请",
         "return": "返回",
         "or_search_for_related_content": "或搜索相关内容",
