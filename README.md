8a77e5ddadea576d5a7cabd5a4719dd02c7e3d0d
Xmader <xmader@outlook.com>
2018-12-12 20:57:13 +0800

    增加中文语言包

---
+++ README.md
@@ -0,0 +1,11 @@
+
+# ZettaForum/language-zh_CN
+
+> ZettaForum的简体中文语言包
+
+
+## 安装 
+
+```bash
+npm install @zetta-forum/language-zh-cn
+```
---
+++ zh_CN.js
@@ -0,0 +1,19 @@
+
+export const translations = {
+    "404page": {
+        "page_not_found": "你访问的页面不存在",
+        "please": "请",
+        "return": "返回",
+        "or_search_for_related_content": "或搜索相关内容",
+    },
+    "search-box": {
+        "placeholder": "搜索问题、话题或人",
+        "search": "搜索",
+    },
+}
+
+export default translations
+
+if (top && top.ZettaForum) {
+    top.ZettaForum.addTranslations("zh_CN", translations)
+}
